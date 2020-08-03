324b1b7a320a42f3c004980d8e14ca09d88704e3
Nicole (Deborah) Marcum <marcum531@gmail.com>
2020-08-03 07:27:19 -0400

    seventh upload

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,7 +1,24 @@
-from flask import Flask, render_template
-import pymongo
-import Mission_to_Mars.py
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import Mission_to_Mars
 
 app = Flask (__name__)
-mongo = pymongo(app, url="mongodb://localhost:27017")
 
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mars_app'
+mongo = PyMongo(app)
+
+@app.route("/")
+def home():
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+@app.route('/scrape')
+def scrape():
+
+    mars = mongo.db.mars
+    mars_facts = Mission_to_Mars.scrape()
+    mars.update({}, mars_facts, upsert=True)
+    return redirect("http://localhost:5000/", code=302)
+
+if __name__=='__main__':
+    app.run(debug=True)
